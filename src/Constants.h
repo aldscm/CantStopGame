f8cf908f05368f9b233c389519633da94826f635
Ana <aldscm7@gmail.com>
2021-01-11 21:47:53 +0000

    Isolate all the constants in a single .h file

---
+++ src/Constants.h
@@ -0,0 +1,20 @@
+#ifndef CONSTANTS_H // protectes from multiple including...
+#define CONSTANTS_H_H // ... needs an #endif at bottom
+
+// JOGADOR
+#define MAX_PLAYER_NAME_INPUT 50
+#define JOGADOR_TIPO_HUMANO 1
+#define JOGADOR_TIPO_BOT 2
+#define MEU_TURNO 1
+#define NAO_E_MEU_TURNO 0
+
+// DADOS
+#define DADO_MIN_NUMBER 1
+#define DADO_MAX_NUMBER 6
+#define NUMERO_DE_DADOS 4
+
+// MATRIZ DE JOGADAS
+#define MATRIZ_JOGADAS_LINHA 3
+#define MATRIZ_JOGADAS_COLUNA 2
+
+#endif
