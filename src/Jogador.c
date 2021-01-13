e363aa9a68805bd6ff936d8cc6e55d1315db900b
Ana <aldscm7@gmail.com>
2021-01-13 22:27:59 +0000

    Add "ARRAY_PERCURSO_SIZE" to Constants.h

--- src/Jogador.c
+++ src/Jogador.c
@@ -9,10 +9,10 @@ void NovoJogador(Jogador *jogador)
 {
     jogador->turno = NAO_E_MEU_TURNO;
     jogador->tipo = JOGADOR_TIPO_HUMANO;
-       
+
    	jogador->percurso = (int*)malloc(sizeof(int));
-    jogador->percursoAvancado = (int*)malloc(sizeof(int)); 
-	
+    jogador->percursoAvancado = (int*)malloc(sizeof(int));
+
 	// Inicializa os percursos a zero
     InicializarPercursos(jogador);
 
@@ -27,115 +27,115 @@ void NovoBot(Jogador *bot)
 {
     bot->turno = NAO_E_MEU_TURNO;
     bot->tipo = JOGADOR_TIPO_BOT;
-    
+
     bot->percurso = (int*)malloc(sizeof(int));
-    bot->percursoAvancado = (int*)malloc(sizeof(int)); 
-       
+    bot->percursoAvancado = (int*)malloc(sizeof(int));
+
 	// Inicializa os percursos a zero
     InicializarPercursos(bot);
-  
+
     strcpy(bot->nome, "Mr.Bot");
 }
 
 // Inicializa os percurso a zero
-void InicializarPercursos(Jogador *jogador) 
+void InicializarPercursos(Jogador *jogador)
 {
     int i;
-   
-    for (i = 0; i< 11; i++) 
+
+    for (i = 0; i< ARRAY_PERCURSO_SIZE; i++)
 	{
-		jogador->percurso = (int*) realloc(jogador->percurso, (i + 1)* sizeof(int));	
+		jogador->percurso = (int*) realloc(jogador->percurso, (i + 1)* sizeof(int));
 		jogador->percurso[i] = 0;
-		
+
 		jogador->percursoAvancado = (int*) realloc(jogador->percursoAvancado, (i + 1)* sizeof(int));
 		jogador->percursoAvancado[i] = 0;
     }
 }
 
-// Copiar percurso avançado para percurso
+// Copiar percurso avanï¿½ado para percurso
 void CopiarPercursoAvancadoParaPercurso(Jogador *jogador)
 {
 	int i;
-	   
-    for (i = 0; i< 11; i++) 
+
+    for (i = 0; i< ARRAY_PERCURSO_SIZE; i++)
 	{
-		jogador->percurso[i] += jogador->percursoAvancado[i];	
+		jogador->percurso[i] += jogador->percursoAvancado[i];
 	}
 }
 
-// Faz reset ao percurso avançado
-void ResetPercursoAvancado(Jogador *jogador) 
+// Faz reset ao percurso avanï¿½ado
+void ResetPercursoAvancado(Jogador *jogador)
 {
 	int i;
-   
-    for (i = 0; i< 11; i++) 
+
+    for (i = 0; i< ARRAY_PERCURSO_SIZE; i++)
 	{
 		jogador->percursoAvancado[i] = 0;
 	}
 }
 
 // Seleciona a jogada para o Bot
-char BotSelecionaJogada(Jogador *bot) 
+char BotSelecionaJogada(Jogador *bot)
 {
 	gotoxy(104, 14);
-	printf(" %s está a pensar...          ", bot->nome);
+	printf(" %s esta a pensar...          ", bot->nome);
 	sleep(2);
-	
+
 	int jogadaSelecionada = NumAleatorio(1, 3);
-	
-	if (jogadaSelecionada == 1) 
+
+	if (jogadaSelecionada == 1)
 	{
 		gotoxy(104, 14);
 		printf(" %s escolhe a opcao 1.", bot->nome);
 		sleep(2);
-		return '1';	
+		return '1';
 	}
 
-	if (jogadaSelecionada == 2) 
+	if (jogadaSelecionada == 2)
 	{
 		gotoxy(104, 14);
 		printf(" %s escolhe a opcao 2.\n", bot->nome);
 		sleep(2);
-		return '2';	
+		return '2';
 	}
-	
-	if (jogadaSelecionada == 3) 
+
+	if (jogadaSelecionada == 3)
 	{
 		gotoxy(104, 14);
 		printf(" %s escolhe a opcao 3.\n", bot->nome);
 		sleep(2);
-		return '3';	
+		return '3';
 	}
 }
 
 // Decide o Bot continua a jogar ou finaliza o turno
-char BotDecideSeContinuaAJogar(Jogador *bot) 
+char BotDecideSeContinuaAJogar(Jogador *bot)
 {
 	int s, n;
-	
+
 	gotoxy(104, 14);
-	printf(" %s está a pensar...          ", bot->nome);
+	printf(" %s esta a pensar...          ", bot->nome);
 	sleep(2);
-	
-	do 
+
+	do
 	{
 		s = NumAleatorio(1, 5);
 		n = NumAleatorio(1, 5);
 	} while(s == n);
-	
-	if (s > n) 
+
+	if (s > n)
 	{
 		gotoxy(104, 14);
 		printf(" %s decide continuar...", bot->nome);
 		sleep(2);
-		return 's';	
-	} 
-	else 
+		return 's';
+	}
+	else
 	{
 		gotoxy(104, 14);
 		printf(" %s decide parar por aqui...", bot->nome);
 		sleep(2);
-		return 'n';	
-	} 
+		return 'n';
+	}
 }
 
--- src/Tabuleiro.c
+++ src/Tabuleiro.c
@@ -7,17 +7,17 @@
 void DesenharTabuleiro(Jogador *jogador, Jogador *bot)
 {
 	int k;
-	int percurso[11] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-    int topo[11] = {3, 5, 7, 9, 11, 13, 11, 9, 7, 5, 3};
-     
+	int percurso[ARRAY_PERCURSO_SIZE] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    int topo[ARRAY_PERCURSO_SIZE] = {3, 5, 7, 9, 11, 13, 11, 9, 7, 5, 3};
+
     // Desenha o tabuleiro
-    for (k = 0; k <= 10; k++)
+    for (k = 0; k < ARRAY_PERCURSO_SIZE; k++)
     {
         DesenharLinha(percurso[k], topo[k]);
     }
-    
-	// Desenha no tabuleiro as fichas para o percurso do jogador 
-	DesenharFichasDoPercurso(jogador); 
+
+	// Desenha no tabuleiro as fichas para o percurso do jogador
+	DesenharFichasDoPercurso(jogador);
 	DesenharFichasDoPercurso(bot);
 }
 
@@ -35,41 +35,41 @@ void DesenharLinha(int num, int limite) // num = nr escrito dentro do retangulo;
 }
 
 // Desenha as fichas para o percurso do jogador
-void DesenharFichasDoPercurso(Jogador *jogador) 
+void DesenharFichasDoPercurso(Jogador *jogador)
 {
 	int i;
-	
-	for(i=0; i <= 10; i++){
+
+	for(i=0; i < ARRAY_PERCURSO_SIZE; i++){
 
 		if (jogador->percurso[i] != 0 )
-		{		
-			setColor(12 - (jogador->tipo - 1), 12 - (jogador->tipo - 1));			
+		{
+			setColor(12 - (jogador->tipo - 1), 12 - (jogador->tipo - 1));
 			showRectAt(4 + 3 * (jogador->tipo - 1) + 8 * (jogador->percurso[i] - 1), 4 + 4 * i, 1, 0);
-			resetColor();					
-		}	
+			resetColor();
+		}
 	}
 }
 
 // Desenha as fichas para o percurso avancado do jogador
-void DesenharFichasDoPercursoAvancado(Jogador *jogador) 
+void DesenharFichasDoPercursoAvancado(Jogador *jogador)
 {
 	int i;
-	
-	for (i = 0; i<= 10; i++) 
+
+	for (i = 0; i < ARRAY_PERCURSO_SIZE; i++)
 	{
-		if (jogador->percursoAvancado[i] != 0) 
+		if (jogador->percursoAvancado[i] != 0)
 		{
 			setColor(15, 15);
-			
-			if (jogador->tipo == JOGADOR_TIPO_HUMANO) 
+
+			if (jogador->tipo == JOGADOR_TIPO_HUMANO)
 			{
 				showRectAt(4 + 8 * (jogador->percurso[i] + jogador->percursoAvancado[i] - 1), 4 + 4 * i, 1 ,0);
-			} 
+			}
 			else // BOT
 			{
 				showRectAt(7 + 8 * (jogador->percurso[i] + jogador->percursoAvancado[i] - 1), 4 + 4 * i, 1 ,0);
 			}
-			
+
 			resetColor();
 		}
 	}
